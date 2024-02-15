2069fce5b2eca8e510cd8825c5e7131aa6dd7161
alxwnth <alxwnth@posteo.me>
2024-02-15 18:05:06 +0200

    Some touches

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("CourierNew.ttf");
   eleventyConfig.addPassthroughCopy("bundle.css");
+  eleventyConfig.addPassthroughCopy({ "favicon.ico": "/" });
 };
