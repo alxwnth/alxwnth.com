a212783437a81f130e67bd0dd596c471733eeeb6
Alex <alxwnth@posteo.me>
2024-03-05 07:33:26 +0000

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("favicon.ico");
   return {
     dir: {
-      output: "public",
+      output: "build",
     },
   };
 };
