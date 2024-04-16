28ba2efc1c76d15f402ab5be237daa33f3d92fc8
alxwnth <alxwnth@posteo.me>
2024-04-16 17:55:23 +0300

    imp: Refactor for maintainability

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,14 @@
+const CleanCSS = require("clean-css");
+
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({
-    './CourierNew.ttf': 'CourierNew.ttf',
-    './bundle.css': 'bundle.css',
-    './favicon.ico': 'favicon.ico'
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
   });
-  
+  eleventyConfig.addPassthroughCopy({
+    "./assets/CourierNew.ttf": "CourierNew.ttf",
+    "./assets/favicon.ico": "favicon.ico",
+  });
+
   return {
     dir: {
       output: "build",
