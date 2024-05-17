b40505f6850fb4d30c273bb11fb7b8d986ce698b
alxwnth <alxwnth@posteo.me>
2024-05-17 18:33:21 +0300

    big update

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+const { DateTime } = require("luxon");
 const CleanCSS = require("clean-css");
 
 module.exports = function (eleventyConfig) {
@@ -7,6 +8,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
     "./assets/CourierNew.ttf": "CourierNew.ttf",
     "./assets/favicon.ico": "favicon.ico",
+  });
+  eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
 
   return {
