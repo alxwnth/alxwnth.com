b238ce6b290f0a6323d3efbc004105864569d9c6
Alex <alxwnth@posteo.me>
2024-11-13 19:20:10 +0200

    Migrate to Astro

---
+++ src/components/Email.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  const email = "alxwnth@posteo.me";
+  let visible = $state(false);
+</script>
+
+{#if visible}
+  <a href="mailto:{email}">{email}</a>
+{:else}
+  <button onclick={() => (visible = true)}>[reveal]</button>
+{/if}
