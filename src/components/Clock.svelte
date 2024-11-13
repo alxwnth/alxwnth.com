bc3e7adc762752ab2a738ca7f2131cee44d31396
Alex <alxwnth@posteo.me>
2024-11-13 19:20:45 +0200

    astro

---
+++ src/components/Clock.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  let time = $state(new Date().toLocaleTimeString());
+
+  const updateTime = () => {
+    time = new Date().toLocaleTimeString();
+  };
+
+  onMount(() => {
+    const interval = setInterval(updateTime, 1000);
+    return () => clearInterval(interval);
+  });
+</script>
+
+<div class="clock">{time}</div>
+
+<style>
+  .clock {
+    padding: 6px;
+    border: 2px solid;
+    border-color: var(--primary-darker) var(--primary-light)
+      var(--primary-light) var(--primary-darker);
+    cursor: default;
+  }
+</style>
